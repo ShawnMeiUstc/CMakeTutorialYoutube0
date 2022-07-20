49f820aaaf4400dd964fe118c426d6fa225c8312
ShawnMei <mrmei1991@163.com>
2022-07-20 11:00:09 +0800

    optional libraries

--- main.cpp
+++ main.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
-#include <adder.h>
-#include <GLFW/glfw3.h>
-
 #include "HelloCmakeConfig.h"
+
+#ifdef USE_ADDER
+#include <adder.h>
+#endif
+
+#ifdef USE_GLFW
+#include <GLFW/glfw3.h>
+#endif
 
 int main(int argc, char **argv)
 {
     std::cout << "Hello Cmake!" << std::endl;
-    std::cout << add(1.0, 3.5) << std::endl;
+#ifdef USE_ADDER
+    std::cout << "Use adder library" << add(1.0, 3.5) << std::endl;
+#endif
     std::cout << "The " << argv[0] << " Version" << HelloCmake_VERSION_MAJOR << "." << HelloCmake_VERSION_MINOR << std::endl;
+#ifdef USE_GLFW
 
-    GLFWwindow* window;
-    if( !glfwInit() )
+    GLFWwindow *window;
+    if (!glfwInit())
     {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
     }
-    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    window = glfwCreateWindow(300, 300, "ForFun", NULL, NULL);
     if (!window)
     {
-        fprintf( stderr, "Failed to open GLFW window\n" );
+        fprintf(stderr, "Failed to open GLFW window\n");
         glfwTerminate();
-        exit( EXIT_FAILURE );
+        exit(EXIT_FAILURE);
     }
-    while( !glfwWindowShouldClose(window) )
+    while (!glfwWindowShouldClose(window))
     {
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+#endif
 }
